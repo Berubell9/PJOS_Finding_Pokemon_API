829de76f0a05534595541060b739bbed87eb87ee
Klinglunlun <79560837+Berubell9@users.noreply.github.com>
2023-10-19 03:08:52 +0700

    os

--- script.js
+++ script.js
@@ -11,14 +11,45 @@ function lowerCaseName(string) {
 function getPokemon(e) {
   const name = document.querySelector("#pokemonName").value;
   const pokemonName = lowerCaseName(name);
+  // const typeColors = {
+  //   electric: '#FFEA70',
+  //   normal: '#B09398',
+  //   fire: '#FF675C',
+  //   water: '#0596C7',
+  //   ice: '#AFEAFD',
+  //   rock: '#999799',
+  //   flying: '#7AE7C7',
+  //   grass: '#4A9681',
+  //   psychic: '#FFC6D9',
+  //   ghost: '#561D25',
+  //   bug: '#A2FAA3',
+  //   poison: '#795663',
+  //   ground: '#D2B074',
+  //   dragon: '#DA627D',
+  //   steel: '#1D8A99',
+  //   fighting: '#2F2F2F',
+  //   default: '#2A1A1F',
+  // };
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then((response) => response.json())
     .then((data) => {
-      let text = "";
+      const text = [];
+      // const color = [];
       for(let i = 0; i < data.types.length; i++){
-        text += data.types[i].type.name + " ";
+        // color[i] = typeColors[data.types[i].type.name ];
+        text.push(" "+data.types[i].type.name+" ");
+        // text += data.types[i].type.name + " ";
       }
       document.getElementById("type").innerHTML = text;
+
+      let textStats = " ";
+      textStats += "💛 "+data.stats[0].stat.name + " : "+ data.stats[0].base_stat +"<br>";
+      textStats += "👊 "+data.stats[1].stat.name + " : "+ data.stats[1].base_stat +"<br>";
+      textStats += "💪 "+data.stats[2].stat.name + " : "+ data.stats[2].base_stat +"<br>";
+      textStats += "⚡ "+data.stats[3].stat.name + " : "+ data.stats[3].base_stat +"<br>";
+      textStats += "⭐ "+data.stats[4].stat.name + " : "+ data.stats[4].base_stat +"<br>";
+      textStats += "🏃‍♂️ "+data.stats[5].stat.name + " : "+ data.stats[5].base_stat +"<br>";
+      document.getElementById("stats").innerHTML = textStats;
       
       document.querySelector(".pokemonBox").innerHTML = `
       <div class="BG">
@@ -29,7 +60,13 @@ function getPokemon(e) {
       </div>
       <div class="pokemonInfos">
         <h1>${capitalizeFirstLetter(data.name)}</h1>
-        <p>Type : ${text}</p>
+        <p id="type">Pokémon type : ${text}</p>
+      </div>
+      `;
+
+      document.querySelector(".pokemonStats").innerHTML = `
+      <div class="pokemonStats">
+        <p id="stats">${textStats}</p>
       </div>`;
     })
     .catch((err) => {
