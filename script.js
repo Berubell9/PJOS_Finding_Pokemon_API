2cb31cfcf5f2a7d6620583217472841d57cc2e55
Klinglunlun <79560837+Berubell9@users.noreply.github.com>
2023-10-19 11:07:38 +0700

    os

--- script.js
+++ script.js
@@ -33,14 +33,14 @@ function getPokemon(e) {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then((response) => response.json())
     .then((data) => {
-      const text = [];
+      const textType = [];
       // const color = [];
       for(let i = 0; i < data.types.length; i++){
         // color[i] = typeColors[data.types[i].type.name ];
-        text.push(" "+data.types[i].type.name+" ");
+        textType.push(" "+data.types[i].type.name+" ");
         // text += data.types[i].type.name + " ";
       }
-      document.getElementById("type").innerHTML = text;
+      document.getElementById("type").innerHTML = textType;
 
       let textStats = " ";
       textStats += "💛 "+data.stats[0].stat.name + " : "+ data.stats[0].base_stat +"<br>";
@@ -49,8 +49,14 @@ function getPokemon(e) {
       textStats += "⚡ "+data.stats[3].stat.name + " : "+ data.stats[3].base_stat +"<br>";
       textStats += "⭐ "+data.stats[4].stat.name + " : "+ data.stats[4].base_stat +"<br>";
       textStats += "🏃‍♂️ "+data.stats[5].stat.name + " : "+ data.stats[5].base_stat +"<br>";
-      document.getElementById("stats").innerHTML = textStats;
       
+      let textAbility = [];
+      for(let i=0;i<data.abilities.length;i++){
+        textAbility.push(" "+data.abilities[i].ability.name+" ");
+      }
+      textStats += "✨ ability"+ " : "+textAbility;
+      document.getElementById("stats").innerHTML = textStats;
+
       document.querySelector(".pokemonBox").innerHTML = `
       <div class="BG">
         <img
@@ -60,7 +66,7 @@ function getPokemon(e) {
       </div>
       <div class="pokemonInfos">
         <h1>${capitalizeFirstLetter(data.name)}</h1>
-        <p id="type">Pokémon type : ${text}</p>
+        <p id="type">Pokémon type : ${textType}</p>
       </div>
       `;
 
@@ -71,9 +77,13 @@ function getPokemon(e) {
     })
     .catch((err) => {
       document.querySelector(".pokemonBox").innerHTML = `
-      <h4>Pokemon not found!!</h4>
+        <h4>Pokémon not found!!</h4>
       `;
-      console.log("Pokemon not found!!", err);
+      console.log("Pokémon not found!!", err);
+      document.querySelector(".pokemonStats").innerHTML = `
+        <img src="https://scontent.fbkk7-3.fna.fbcdn.net/v/t39.30808-6/317554971_599606642113247_22300277896278755_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG6fuNN66rdutHLhJwdlukECEDvk8ymPkgIQO-TzKY-SCQD5F64hqrZ7uEzWeluxyz8DS3CBtQxTtbYjd8apSBk&_nc_ohc=dlHIdsEo9EsAX8hoSC8&_nc_ht=scontent.fbkk7-3.fna&_nc_e2o=s&oh=00_AfB5kUhS-EaskSJAtlADE7h5Ipo4n_edBJSEcP8uUFgrlQ&oe=653615C0" width="500px">
+      `;
     });
+    
   e.preventDefault();
 }
